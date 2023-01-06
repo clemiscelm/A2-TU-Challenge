08a11b6f63aec333c7465d6997550729c2b249bb
clément bostyn <clemclem20002@gmail.com>
2023-01-06 14:26:08 +0100

    ajout dans les strings test

--- TU Challenge/MyStringImplementation.cs
+++ TU Challenge/MyStringImplementation.cs
@@ -10,7 +10,7 @@ namespace TU_Challenge
     {
         public static bool IsNullEmptyOrWhiteSpace(string input)
         {
-            if (input.Length == 0)
+            if (input == null || input.Length == 0 )
                 return true;
             for(int i = 0; i < input.Length; i++)
             {
@@ -22,12 +22,55 @@ namespace TU_Challenge
 
         public static string MixString(string a, string b)
         {
-            throw new NotImplementedException();
+            
+            if(a == null || a.Length == 0 || b == null || b.Length == 0)
+                throw new System.ArgumentException("Logfile cannot be read-only");
+            int longueur;
+            if (a.Length >= b.Length)
+                longueur = a.Length;
+            else
+                longueur = b.Length;
+
+
+            string resultat = "";
+            for (int i = 0; i < longueur; i++)
+            {
+                if (i < a.Length)
+                {
+                    resultat += a[i];
+                }
+
+                if (i < b.Length)
+                {
+                    resultat += b[i];
+                }
+            }
+
+            return resultat;
         }
 
         public static string ToLowerCase(string a)
         {
-            throw new NotImplementedException();
+
+            char[] resultat = new char[a.Length];
+            // Parcourt tous les caractères de la chaîne
+            for (int i = 0; i < a.Length; i++)
+            {
+                // Convertit le caractère en minuscule et l'ajoute au tableau
+                //resultat[i] = Char.ToLower(a[i]);
+                if (a[i] >= 'A' && a[i] <= 'Z')
+                {
+                    resultat[i] = (char)(a[i] + ('a' - 'A'));
+                }
+                else
+
+                {
+                    resultat[i] = a[i];
+                }
+            }
+            string res = string.Join("", resultat);
+            return res;
+            
         }
 
         public static string Voyelles(string a)
